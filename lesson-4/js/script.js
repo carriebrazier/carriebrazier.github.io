5ecb79140676641b1145073f7214d2a87c17e233
carriebrazier <bra18048@byui.edu>
2019-10-12 16:55:15 -0500

    new text

--- lesson-4/js/script.js
+++ lesson-4/js/script.js
@@ -1,3 +1,9 @@
 function toggleMenu() {
 	document.getElementsByClassName("navigation")[0].classList.toggle("responsive");
 }
+var date = new Date();
+document.getElementById("year").textContent = date.getFullYear();
+
+var month = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+var day = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+ document.getElementById("date").textContent = day[date.getDay()] + ", " + date.getDate() + " " + month[date.getMonth()] + " " + date.getFullYear();
