2ce60d1f5a65e1940b17549f68160abd886235ff
carriebrazier <bra18048@byui.edu>
2019-11-08 16:54:13 -0600

    newest

--- lesson-7/js/js.js
+++ lesson-7/js/js.js
@@ -1,4 +1,4 @@
- const imagesToLoad = document.querySelectorAll('img[data-src]');
+const imagesToLoad = document.querySelectorAll('img[data-src]');
 
 const imgOptions={
     threshold:3,
@@ -41,15 +41,5 @@ var day = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Sa
 
 new Date().getDay() == 5 ? document.getElementById('reminder').innerHTML = "Saturday = Preston Pancakes in the Park!  9:00 a.m. Saturday at the city park pavilion." : null ;
 
-function doInputOutput() {
-	var t = parseFloat(document.getElementById('temperature').value);
-	var s = parseFloat(document.getElementById('windSpeed').value);
-	var f = windChill(t, s);
-	   document.getElementById('outputDiv').innerHTML = f;
+
  
- }
- function windChill(tempF, speed) {
-	 var f = (35.74 + (0.6215 * tempF)) - (35.75 * Math.pow(speed,0.16)) + (0.4275 * tempF * Math.pow(speed,0.16));
-	 return f
-	
- }
