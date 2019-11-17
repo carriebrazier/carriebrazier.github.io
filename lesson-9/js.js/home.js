a4e2fb3c2336591cab49433a0c584bf32e9851d5
carriebrazier <bra18048@byui.edu>
2019-11-16 20:32:56 -0600

    some text

--- lesson-9/js.js/home.js
+++ lesson-9/js.js/home.js
@@ -1,39 +1,4 @@
-/*const requestURL = 'https://byui-cit230.github.io/weather/data/towndata.json';
-fetch(requestURL)
-  .then(function (response) {
-    return response.json();
-  })
-  .then(function (jsonObject) {
-    //console.table(jsonObject);  // temporary checking for valid response and data parsing
-    const towns = jsonObject['towns'];
-    for (let i = 0; i < towns.length; i++ ) {
-        if (towns[i].name == 'Fish Haven' || towns[i].name == 'Preston' || towns[i].name == 'Soda Springs'){
-        let card = document.createElement('section');
-        let h2 = document.createElement('h2');
-        let motto = document.createElement('p');
-        let year = document.createElement('p');
-        let population = document.createElement('p');
-        let rainfall = document.createElement('p');
-        let image = document.createElement('img');
 
-        h2.textContent = towns[i].name;
-        motto.textContent = towns[i].motto;
-        year.textContent = 'Founded in ' + towns[i].yearFounded;
-        population.textContent = 'Current Population: ' + towns[i].currentPopulation;
-        rainfall.textContent = 'Average Rainfall: ' + towns[i].averageRainfall;
-        image.setAttribute('src', 'images/' + towns[i].photo);
-        image.setAttribute('alt', prophets[i].name + ' ' + prophets[i].lastname);
-        
-        card.appendChild(h2);
-        card.appendChild(motto);
-        card.appendChild(year);
-        card.appendChild(population);
-        card.appendChild(rainfall);
-        card.appendChild(image);
-        document.querySelector('div.towns').appendChild(card);
-    }
-}
-  });//*/
   const requestURL = 'https://byui-cit230.github.io/weather/data/towndata.json';
 
 fetch(requestURL)
@@ -50,6 +15,7 @@ fetch(requestURL)
         let motto = document.createElement('h3');
         let year = document.createElement('h4');
         let rain = document.createElement('h4');
+        let population = document.createElement('h4');
         let pic = document.createElement('img');
         
         pic.setAttribute('src', 'images/' + towns[i].photo);
@@ -68,6 +34,9 @@ fetch(requestURL)
         rain.textContent = 'Avg. rainfall: ' + towns[i].averageRainfall;
         townpage.appendChild(rain);
         
+        population.textContent = 'Population: ' + towns[i].currentPopulation;
+        townpage.appendChild(population); 
+
         document.querySelector('div.towns').appendChild(townpage);
       }
     }
