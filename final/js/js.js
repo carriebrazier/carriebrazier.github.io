70ac5fde8d93093912addd6b9f6d3ffe02533c25
carriebrazier <bra18048@byui.edu>
2019-12-20 20:09:20 -0600

    some new

--- final/js/js.js
+++ final/js/js.js
@@ -18,7 +18,7 @@ var day = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Sa
     
       const towns = jsonObject['towns'];
       for (let i = 0; i < towns.length; i++ ) {
-        if (towns[i].name == 'St.Louis Missouri Temple' || towns[i].name == 'San Diego, California Temple' || towns[i].name == 'Nauvoo, Illinois Temple' || towns[i].name == 'Kansas City, Missouri Temple'){
+        if (towns[i].name == 'St.Louis Missouri Temple'){
           let townpage = document.createElement('section');
           let townname = document.createElement('h1');
           let address = document.createElement('h4');
@@ -59,3 +59,42 @@ var day = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Sa
         window.location.href = "thanks.html";
     
     }
+
+   const townId = document.getElementById('town').dataset.townid;
+console.log(townId);
+
+// const prestonId = 5604473;
+const appId = "9cabcd53024a11057d12175fbbcc9858";
+const weatherAPIURL = `https://api.openweathermap.org/data/2.5/weather?${townId}&units=imperial&APPID=${appId}`;
+const forecastAPIURL = `https://api.openweathermap.org/data/2.5/forecast?id=${townId}&units=imperial&APPID=${appId}`;
+const weatherIconPath = "https://openweathermap.org/img/w/";
+const dow = new Intl.DateTimeFormat("en-US", {
+   weekday: "short"
+});
+
+fetch(weatherAPIURL)
+   .then((response) => response.json())
+   .then((jsObject) => {
+      console.log(jsObject);
+      const tempF = jsObject.main.temp.toFixed(0);
+      const windSpeedMPH = jsObject.wind.speed.toPrecision(2);
+      const windChillF = windChill(tempF, windSpeedMPH);
+      document.getElementById('temperature').textContent = tempF;
+      document.getElementById('hightemp').textContent = jsObject.main.temp_max.toFixed(0);
+      document.getElementById('windspeed').textContent = windSpeedMPH;
+      document.getElementById('windchill').textContent = windChillF;
+      document.getElementById('humidity').textContent = jsObject.main.humidity.toFixed(0);
+      document.getElementById('description').textContent = jsObject.weather[0].description;
+   });
+
+function windChill(tempF, speed) {
+   const maxTempF = 50;
+   const minWindSpeed = 3;
+   let f = "N/A"
+   if ((tempF <= maxTempF) && (speed >= minWindSpeed)) {
+      f = (35.74 + (0.6215 * tempF) -
+         (35.75 * speed ** 0.16) +
+         (0.4275 * tempF * speed ** 0.16)).toFixed(0);
+   }
+   return f;
+}
